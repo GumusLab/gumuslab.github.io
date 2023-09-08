36b9dd4afad34d924d13310659d1f9466cc7fea4
oshorawal <oshorawal26@gmail.com>
2023-09-08 17:11:18 -0400

    added the new proxy server

--- src/publications.js
+++ src/publications.js
@@ -1,3 +1,154 @@
+// // Create a cache object for the initial API call
+// const apiCache = {};
+
+// // Create a cache object for the Crossref API calls
+// const crossrefCache = {};
+
+// const apiURL = "https://pub.orcid.org/v2.0/0000-0002-7364-2202/works";
+
+
+// const publicationInfoElement = document.getElementById("papers");
+// const loadingIndicator = document.getElementById("loading-indicator");
+
+// const monthDict = {
+//     '1':'Jan',
+//     '2':'Feb',
+//     '3':'March',
+//     '4':'April',
+//     '5':'May',
+//     '6':'June',
+//     '7':'July',
+//     '8':'Aug',
+//     '9':'Sept',
+//     '10':'Oct',
+//     '11':'Nov',
+//     '12':'Dec',
+// }
+
+// function generateContentForWork(crossrefWork, paperDoiValue) {
+//   // ... generate content for a work
+//   // Return the content as a string
+//   let content = ""
+//   const title = crossrefWork.title[0] || "Untitled";
+//   const publicationDate = crossrefWork.created["date-parts"][0];
+//   const year = publicationDate[0];
+//   const month = publicationDate[1];
+//   const day = publicationDate[2];
+//   const publisher = crossrefWork.publisher;
+//   content += `<div class="paper">`;
+//   content += `<img src="../static/DNA1.jpeg">`;
+//   content += `<div class="paper-content">`;
+//   content += `<a href="${crossrefWork.URL}" target="_blank">`;
+//   content += `<p class="paper-headline">${title}</p>`;
+//   content += `</a>`;
+//   content += `<p>${publisher} · ${day} ${monthDict[month]} ${year} · doi:${paperDoiValue}</p>`;
+//   content += `</div>`;
+//   content += `<h1>${year}</h1>`;
+//   content += `</div>`;
+
+//   return content
+// }
+
+// // Define a function to fetch data from the API with memoization
+// function fetchWithMemoization(url, headers, cache) {
+//   if (cache[url]) {
+//     // If the data is already in the cache, return it as a resolved promise
+//     return Promise.resolve(cache[url]);
+//   } else {
+//     // If not in the cache, fetch the data and store it in the cache
+//     return fetch(url, { headers })
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error(`Request failed with status ${response.status}`);
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         // Store the data in the cache
+//         cache[url] = data;
+//         return data;
+//       })
+//       .catch((error) => {
+//         console.error("API Error:", error);
+//         throw error; // Propagate the error
+//       });
+//   }
+// }
+
+// // Make the initial API request using the fetchWithMemoization function
+// fetchWithMemoization(apiURL, headers = {
+//   "Accept": "application/json"
+// }, apiCache)
+//   .then((data) => {
+//     // Handle the JSON data
+//     const works = data.group || []; // Assuming "group" is the key in the response
+//     let content = "";
+//     const fetchPromises = works.map((work) => {
+//       const workSummary = work["work-summary"][0] || {}; // Assuming "work-summary" is the key in the response
+//       const paperDoi = workSummary["external-ids"]["external-id"].filter(
+//         (obj) => obj["external-id-type"] == "doi"
+//       );
+
+//       if (paperDoi.length > 0) {
+//         const paperDoiValue = paperDoi[0]["external-id-value"];
+//         // const corsAnywhereUrl =
+//         //   "https://radiant-stream-45835-7a55665f22ae.herokuapp.com/";
+//         const corsAnywhereUrl = "https://cors-anywhere-iota-two.vercel.app/"
+//         const crossrefApiUrl = `https://api.crossref.org/works/${paperDoiValue}`;
+//         const fullCrossrefApiUrl = `${corsAnywhereUrl}${crossrefApiUrl}`;
+
+//         // Use fetchWithMemoization to fetch the Crossref data
+//         return fetchWithMemoization(
+//           fullCrossrefApiUrl,
+//           headers={},
+//           crossrefCache
+//         ).then((crossrefData) => {
+//           const crossrefWork = crossrefData.message;
+//           const publicationDate = crossrefWork.created["date-parts"][0];
+//           const year = publicationDate[0];
+
+//           return {
+//             year,
+//             content: generateContentForWork(crossrefWork, paperDoiValue),
+//           };
+//         });
+//       }
+//     });
+
+//     const validFetchPromises = fetchPromises.filter((promise) => promise);
+    
+//     console.log(validFetchPromises);
+
+
+//     if (validFetchPromises.length > 0) {
+//       Promise.all(validFetchPromises)
+//         .then((results) => {
+//           console.log('Hola I am isnide')
+//           loadingIndicator.style.display = "none";
+//           // const validResults = results.filter((result) => result);
+//           const sortedResults = results.sort((a, b) => b.year - a.year);
+
+//           // Update the content with sorted data
+//           sortedResults.forEach((result) => {
+//             content += result.content;
+//           });
+
+//           // Set the innerHTML of publicationInfoElement
+//           publicationInfoElement.innerHTML = content;
+//         })
+//         .catch((error) => {
+//           console.error("Error:", error);
+//         });
+//     } else {
+//       // Handle the case where no papers with DOIs were found
+//       loadingIndicator.style.display = "none";
+//       publicationInfoElement.innerHTML = "No papers with DOIs found.";
+//     }
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+
 // API URL
 const apiURL = "https://pub.orcid.org/v2.0/0000-0002-7364-2202/works";
 // const crossrefApiUrl = `https://api.crossref.org/works/${doi}`;
@@ -71,11 +222,11 @@ fetch(apiURL, { headers })
         if(paperDoi.length > 0){
             const paperDoiValue = paperDoi[0]["external-id-value"];
             // const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/";
-            const corsAnywhereUrl = "https://radiant-stream-45835-7a55665f22ae.herokuapp.com/"
+            const corsAnywhereUrl = "https://cors-proxy-server-rust.vercel.app/api"
             const crossrefApiUrl = `https://api.crossref.org/works/${paperDoiValue}`;
-            const fullCrossrefApiUrl = `${corsAnywhereUrl}${crossrefApiUrl}`;
+            const fullCrossrefApiUrl = `${corsAnywhereUrl}?url=${crossrefApiUrl}`;
             return fetch(fullCrossrefApiUrl)
-                .then(response => {
+              .then(response => {
                 if (!response.ok) {
                   throw new Error(`Request failed with status ${response.status}`);
                 }
@@ -123,22 +274,3 @@ fetch(apiURL, { headers })
     console.error("Error:", error);
   });
 
-
-
-
-   // content += "<div class=paper><img src=https://media.springernature.com/lw685/springer-static/image/art%3A10.1038%2Fs41698-023-00354-3/MediaObjects/41698_2023_354_Fig2_HTML.png?>"
-            // const paperSrc = paperDoi[0]["external-id-url"] ? paperDoi[0]["external-id-url"].value:null;
-    
-            // const title = workSummary.title.title.value || "Untitled"; // Assuming "title" is the key in the work summary
-            // const publicationDay = workSummary["publication-date"].day ? workSummary["publication-date"].day.value:null;
-            // const publicationMonth = workSummary["publication-date"].month ? monthDict[workSummary["publication-date"].month.value]:null;
-            // const publicationYear = workSummary["publication-date"].year ?  workSummary["publication-date"].year.value:null;
-            // const articleType = workSummary.type;
-    
-            // content += 
-            // `<div class=paper-content>
-            // <a href=${paperSrc} target="_blank">
-            // <p class="paper-headline">${title}</p>
-            // </a>`
-            // content += `<p>${articleType}   ·   ${publicationDay} ${publicationMonth} ${publicationYear}   ·   doi:${paperDoiValue}</p></div>`
-            // content += `<h1>${publicationYear}</h1></div>`
